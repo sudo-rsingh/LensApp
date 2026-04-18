347bddf2a35641b45e6fd5590598324321a4baf8
Rakshit Kumar Singh <rakshitsingh421@gmail.com>
2026-04-18 17:58:29 +0530

    path

--- .detoxrc.js
+++ .detoxrc.js
@@ -13,6 +13,8 @@ module.exports = {
     'android.debug': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
+      testBinaryPath:
+        'android/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk',
       build:
         'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug --no-daemon',
       reversePorts: [8081],
